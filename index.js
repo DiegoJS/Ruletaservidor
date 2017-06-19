aa21dfcc71d9abaa509d0232ff4b615dfaf69981
Diego JD <diego.g20x@gmail.com>
2017-06-18 23:50:35 -0500

    Eliminado el codigo comentado
    Quitados codigo innecesario

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-/*var express = require("express"),
-	app = express(),
-	server = require("http").createServer(app),
-	io = require("socket.io").listen(server);
-server.listen(3000);
-app.get("/", function(req, res) { 
-	res.sendFile(__dirname + "/public/cliente.html");
-});
-io.sockets.on("connection", function(socket) { 
-	console.log("Se haconectado un nuevo cliente al puerto 3000");
-});*/
-/**************/
 var app = require('express')();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server, { log: false });
